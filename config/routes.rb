de7e5c411875da7b97350f134449e2f1e183e430
vagrant <vagrant@localhost.localdomain>
2020-07-05 05:44:29 +0000

    Add follow function

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  get 'follow/:user_id' => 'relationships#follow', as: 'follow'
+  get 'follower/:user_id' => 'relationships#follower', as: 'follower'
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :edit, :update, :index]
+  resources :users, only: [:show, :edit, :update, :index] do
+    resource :relationships, only: [:create, :destroy]
+  end
 end
